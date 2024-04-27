6c56e62a7aa5b331251983f16ace31545ed05b51
mahajankapil005 <130069883+mahajankapil005@users.noreply.github.com>
2024-04-27 15:16:05 +0000

    changes done

--- js/01_Basics.js
+++ js/01_Basics.js
@@ -1,1 +1,8 @@
-console.log("hi")
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+
+let myCreatedDate = new Date(2024, 4, 27)
+console.log(myCreatedDate);
