eff61680948b90eb809a2cb0834f3854d332423d
mahajankapil005 <130069883+mahajankapil005@users.noreply.github.com>
2024-06-08 17:54:49 +0000

    PROMISES DISCUSSION GOING ON....PART2

--- js/promise.js
+++ js/promise.js
@@ -21,3 +21,63 @@ new Promise(function(resolve,reject){
 }).then(function(){
     console.log("Async task two resolved");
 })
+
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@chai.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(() => {
+        let error = true
+        if(!error){
+            resolve({username: "Hitesh", password: "123"})
+        }else{
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000);
+})
+
+promiseFour
+.then((user)=>{
+    console.log(user);
+    return user.username
+})
+.then((username)=>{
+    console.log(username);
+})
+.catch(function(error){
+    console.log(error);
+})
+.finally(()=>{
+    console.log("The promise is either resolved or rejected");
+})
+
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(() => {
+        let error = true
+        if(!error){
+            resolve({username: "js", password: "123"})
+        }else{
+            reject('ERROR: js went wrong')
+        }
+    }, 1000);
+})
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
